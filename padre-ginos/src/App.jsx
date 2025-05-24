5d4a5efc69b7e08088afbcfbcd4979f5a727ad9d
Hemanth Poluru <poluruhemu@gmail.com>
2025-05-24 17:59:30 +0530

    Added images and stylesheet via API

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,19 +1,29 @@
 import { createRoot } from "react-dom/client";
-import  Pizza from "./Pizza";
-
+import Pizza from "./Pizza";
 
 const App = () => {
-  return(
+  return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="pep, cheese"/>
-      <Pizza name="Hawaiin" description="pep, cheese"/>
-      <Pizza name="Tandoori" description="pep, cheese"/>
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="Hawaiin"
+        description="pep, cheese"
+        image="/public/pizzas/hawaiian.webp"
+      />
+      <Pizza
+        name="Tandoori"
+        description="pep, cheese"
+        image="/public/pizzas/big_meat.webp"
+      />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
- 
+root.render(<App />);
